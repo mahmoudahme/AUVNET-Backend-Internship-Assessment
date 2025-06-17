746be61a5e391622a55443c73cc6f85a72771eda
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2025-06-17 18:16:35 +0300

    Delete utils directory

--- utils/jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (user) => {
-  return jwt.sign({ id: user._id, type: user.type }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
-  });
-};
